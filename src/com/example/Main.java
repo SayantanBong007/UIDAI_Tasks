7b46918cc35b30a62f4fa18ca15968a7ba7d6688
Sayantan Gain <sayantangain63@gmail.com>
2025-07-16 20:24:34 +0530

    Java to ProtoBuf finally done

---
+++ src/com/example/Main.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import com.example.PersonProto.Person;
+
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create Person object
+        Person person = Person.newBuilder()
+                .setName("Alice")
+                .setAge(30)
+                .setBalanceAmount(10000.50)
+                .addInterests("Traveling")
+                .addInterests("Music")
+                .build();
+
+        // Serialize to a Protobuf file
+        try (FileOutputStream fos = new FileOutputStream("person.protobuf")) {
+            person.writeTo(fos);
+            System.out.println("Person object serialized to person.protobuf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Deserialize back
+        try (FileInputStream fis = new FileInputStream("person.protobuf")) {
+            Person deserialized = Person.parseFrom(fis);
+            System.out.println("Deserialized Person:");
+            System.out.println("Name: " + deserialized.getName());
+            System.out.println("Age: " + deserialized.getAge());
+            System.out.println("Balance: " + deserialized.getBalanceAmount());
+            System.out.println("Interests: " + deserialized.getInterestsList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
